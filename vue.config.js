95aeb3acb93114f242e50533ac34bae7a90e839c
Elias Johnson <eliasjohnson211@gmail.com>
2025-03-08 15:38:04 -0700

    fixed config file

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/MyWebApp/' // Replace with your repository name
-    : '/',
+  publicPath: '/MyWebApp/',
   outputDir: 'dist'
 }
